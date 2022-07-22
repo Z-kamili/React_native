1c9b1b9abc3640e7cb75ec4168b260af3a431f5b
Z-Kamili <zakariakamili97@gmail.com>
2022-07-22 16:08:55 +0100

    add more styles

--- App.js
+++ App.js
@@ -1,29 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,Button } from 'react-native';
+import { StyleSheet, Text, View,Button,TextInput } from 'react-native';
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <View>
-          <Text>Another piece of text!</Text>
-      </View>
-      <Text style={styles.dummyText}>Hello World!</Text>
-      <Button title="Press Me"/>
+
+    <View style={styles.appContainer}>
+       
+       <View style={styles.inputContainer}>
+        <TextInput style={styles.TextInput} placeholder='Your course goal!'></TextInput>
+        <Button title="Add Goal"/>
+       </View>
+
+       <View>
+           <Text>List of goals...</Text>
+       </View>
+
     </View>
+
   );
+
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  
+  appContainer:{
+    padding:50,
+    marginTop:20
   },
-  dummyText : {
-     margin:16,
-     padding:16,
-     borderWidth:2,
-     borderColor:'blue',
+
+  inputContainer:{
+     flexDirection:'row',
+     justifyContent:'space-between'
+  },
+
+  TextInput : {
+    borderWidth:1,
+    borderColor:'#cccccc',
+    width:'80%',
+    marginRight:8,
+    padding:8
   }
+
+
+         
+
 });
